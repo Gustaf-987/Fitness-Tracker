2f5d79cdd7b54d78648bba800cd9f453ffa874b8
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-02-25 22:49:29 -0600

    Fixing deploy issues

--- db/workout.js
+++ db/workout.js
@@ -1,24 +1,60 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
 const workoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now,
-        // required: true,
+        required: "Date is Required"
     },
     exercises: [{
-        type: { type: String },
-        name: String,
-        duration: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number,
-        distance: Number,
-        // trim: true,
-        // required: "Exercise is required"
+        type: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        duration: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance: {
+            type: Number,
+        }
     }],
-})
+});
+
+// const workoutSchema = new Schema({
+//     day: {
+//         type: Date,
+//         default: Date.now,
+//         // required: true,
+//     },
+//     exercises: [{
+//         type: { type: String },
+//         name: String,
+//         duration: Number,
+//         weight: Number,
+//         reps: Number,
+//         sets: Number,
+//         distance: Number,
+//         // trim: true,
+//         // required: "Exercise is required"
+//     }],
+// })
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
--- seeders/seed.js
+++ seeders/seed.js
@@ -4,8 +4,8 @@ let db = require("../db");
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout', {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
+        // useUnifiedTopology: true,
+        // useCreateIndex: true,
         useFindAndModify: false
     }
 );
