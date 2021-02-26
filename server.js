4df6eac4132af920f0768f04520f03acc3ce1bb1
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-02-25 23:24:42 -0600

    Deploy fixes

--- server.js
+++ server.js
@@ -1,34 +1,57 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-// const apiroutes = require("./routes/apiroutes.js");
-const PORT = process.env.PORT || 3000;
-
-// const db = require("./models");
-
+const PORT = process.env.PORT || 3003;
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useCreateIndex: true,
-        useFindAndModify: false
-    }
-);
-
+//Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 app.use(require("./routes/apiroutes.js"));
 app.use(require("./routes/htmlroutes.js"));
-// app.use(apiroutes);
-// app.use(htmlroutes);
-
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
+
+
+// const express = require("express");
+// const logger = require("morgan");
+// const mongoose = require("mongoose");
+// // const apiroutes = require("./routes/apiroutes.js");
+// const PORT = process.env.PORT || 3000;
+
+// // const db = require("./models");
+
+// const app = express();
+
+// app.use(logger("dev"));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("public"));
+
+// mongoose.connect(
+//     process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         // useCreateIndex: true,
+//         useFindAndModify: false
+//     }
+// );
+
+// app.use(require("./routes/apiroutes.js"));
+// app.use(require("./routes/htmlroutes.js"));
+// // app.use(apiroutes);
+// // app.use(htmlroutes);
+
+// app.listen(PORT, () => {
+//     console.log(`App running on port ${PORT}!`);
+// });
